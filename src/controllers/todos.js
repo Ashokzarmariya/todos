5ce87b1a6f5c72ac343f8deb96716961ee9b8360
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-02-04 19:09:29 +0530

    add delet method

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -4,6 +4,18 @@ const router = express.Router();
 
 router.get("/get", async (req, res) => {
     try {
+        const page = +req.query._page;
+        const limit = +req.query._limit;
+      
+        const offset = (page - 1) * limit;
+      
+        const posts = await Todos.find({})
+          .sort({ createdAt: 'desc' })
+          .skip(offset)
+          .limit(limit)
+          .lean()
+          .exec();
+      
         const todos = await Todos.find().lean().exec();
         return res.status(200).send(todos);
     }
@@ -15,6 +27,17 @@ router.get("/get", async (req, res) => {
 router.post("/add", async (req, res) => {
     try {
         const todos = await Todos.create(req.body);
+        return res.status(201).send(todos);
+    }
+    catch (err) {
+        return res.status(500).send(err.message);
+    }
+});
+
+router.post("/delet:id", async (req, res) => {
+    try {
+        const todos = await Todos.findByIdAndDelete(req.params.id)
+        //const todos = await Todos.create(req.body);
         return res.status(201).send(todos);
     }
     catch (err) {
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const  {connect}  = require("./configs/db");
 const app = require("./index");
 
-const port = process.env.PORT || 3454;
+const port = process.env.PORT || 3054;
 
 app.listen(port, async (req, res) => {
     await connect()
-    console.log("listing on port 3454");
-})
+    console.log("listing on port 3054");
+})  
